5fe9f12b2ebc1fe87dfaaafa9357d0dcbb7679a3
elbionic <info@elbionic.com>
2024-10-09 23:38:32 +0200

    Add basic math functions in JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber = 3;
+let secondNumber = 2;
+let operator = "+";
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+console.log(add(firstNumber, secondNumber));
+console.log(subtract(firstNumber, secondNumber));
+console.log(multiply(firstNumber, secondNumber));
+console.log(divide(firstNumber, secondNumber));
